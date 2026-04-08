4ea9e72e2ffcbe491c1d4dc064aa027e3c9f0531
= <=>
2026-04-07 23:25:43 -0300

    feat: Notícias mais lidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import bikanews from './assets/bikanews.png'
-import { Search, Bell, User, Clock } from 'lucide-react'
+import { Search, Bell, User, Clock, TrendingUp } from 'lucide-react'
 import tajMahal from './assets/tajMahal.png'
 
 //#fa6832
@@ -44,14 +44,14 @@ function App() {
             </div>
           </div>
         </div>
-        <div className='w-[clamp(500px,80vw,1400px)] h-auto flex flex-row'>
+        <div className='w-[clamp(500px,80vw,1400px)] h-auto flex flex-row gap-12'>
           <div className='flex-2 flex flex-col gap-8'>
-            <div className='flex flex-row border-b-2 border-[#fa6832] pb-2'>
-              <div className='h-auto w-1 bg-[#fa6832] rounded-4xl' />
+            <div className='flex flex-row border-b-2 border-[#fa6832] pb-2 items-center'>
+              <div className='h-full w-2 bg-[#fa6832] rounded-4xl' />
               <h1 className='font-bold p-2 text-xl text-[#fa6832]'>Últimas notícias</h1>
             </div>
             <div className='grid grid-cols-2 w-full h-auto gap-6 flex-col responsive-card'>
-              <div className='flex flex-1 flex-col h-[25svw] shadow-[0_2px_6px_rgba(0,0,0,0.15)] rounded-2xl overflow-hidden group hover:cursor-pointer origin-top hover:shadow-[0_12px_28px_rgba(0,0,0,0.2)] hover:scale-y-[0.98] transition duration-500'>                
+              <div className='flex flex-1 flex-col h-[25svw] shadow-[0_2px_6px_rgba(0,0,0,0.15)] rounded-2xl overflow-hidden group hover:cursor-pointer origin-top hover:shadow-[0_12px_28px_rgba(0,0,0,0.2)] hover:scale-y-[0.98] transition duration-500'>
                 <div className='h-[45%] w-full'>
                   <img className='h-full w-full object-cover group-hover:scale-110 transition-transform duration-500' src={tajMahal} alt="" />
                 </div>
@@ -149,7 +149,81 @@ function App() {
               </div>
             </div>
           </div>
-          <div className='flex-1 bg-amber-900 h-6'></div>
+          <div className='flex-1 flex-col shadow-2xl bg-white h-fit pb-4 rounded-2xl box-border'>
+            <div className='w-full h-full flex flex-col px-10'>
+              <div className='flex flex-row border-b-2 border-[#fa6832] pb-2 items-center'>
+                <TrendingUp className='h-full w-8 text-[#fa6832]' />
+                <h1 className='font-bold p-2 text-xl text-[#fa6832]'>Mais lidas</h1>
+              </div>
+              <div className='flex flex-col'>
+                <div className='flex flex-row group h-32 w-full items-center gap-5'>
+                  <h1 className='text-4xl font-bold text-[#fa67325d]'>01</h1>
+                  <div className='flex flex-col gap-2'>
+                    <h2 className='font-medium'>Pesquisa revela hábitos de bla bla bla bla...</h2>
+                    <div className='flex flex-row gap-2 text-gray-600'>
+                      <Clock className='h-4 w-4' />
+                      <h3 className='text-xs'>Há 1 hora</h3>
+                    </div>
+                  </div>
+                  <div className='h-25 min-w-25 rounded-md overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={tajMahal} alt="" />
+                  </div>
+                </div>
+                <div className='flex flex-row group h-32 w-full items-center gap-5'>
+                  <h1 className='text-4xl font-bold text-[#fa67325d]'>02</h1>
+                  <div className='flex flex-col gap-2'>
+                    <h2 className='font-medium'>Pesquisa revela hábitos de bla bla bla bla...</h2>
+                    <div className='flex flex-row gap-2 text-gray-600'>
+                      <Clock className='h-4 w-4' />
+                      <h3 className='text-xs'>Há 1 hora</h3>
+                    </div>
+                  </div>
+                  <div className='h-25 min-w-25 rounded-md overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={tajMahal} alt="" />
+                  </div>
+                </div>
+                <div className='flex flex-row group h-32 w-full items-center gap-5'>
+                  <h1 className='text-4xl font-bold text-[#fa67325d]'>03</h1>
+                  <div className='flex flex-col gap-2'>
+                    <h2 className='font-medium'>Pesquisa revela hábitos de bla bla bla bla...</h2>
+                    <div className='flex flex-row gap-2 text-gray-600'>
+                      <Clock className='h-4 w-4' />
+                      <h3 className='text-xs'>Há 1 hora</h3>
+                    </div>
+                  </div>
+                  <div className='h-25 min-w-25 rounded-md overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={tajMahal} alt="" />
+                  </div>
+                </div>
+                <div className='flex flex-row group h-32 w-full items-center gap-5'>
+                  <h1 className='text-4xl font-bold text-[#fa67325d]'>04</h1>
+                  <div className='flex flex-col gap-2'>
+                    <h2 className='font-medium'>Pesquisa revela hábitos de bla bla bla bla...</h2>
+                    <div className='flex flex-row gap-2 text-gray-600'>
+                      <Clock className='h-4 w-4' />
+                      <h3 className='text-xs'>Há 1 hora</h3>
+                    </div>
+                  </div>
+                  <div className='h-25 min-w-25 rounded-md overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={tajMahal} alt="" />
+                  </div>
+                </div>
+                <div className='flex flex-row group h-32 w-full items-center gap-5'>
+                  <h1 className='text-4xl font-bold text-[#fa67325d]'>05</h1>
+                  <div className='flex flex-col gap-2'>
+                    <h2 className='font-medium'>Pesquisa revela hábitos de bla bla bla bla...</h2>
+                    <div className='flex flex-row gap-2 text-gray-600'>
+                      <Clock className='h-4 w-4' />
+                      <h3 className='text-xs'>Há 1 hora</h3>
+                    </div>
+                  </div>
+                  <div className='h-25 min-w-25 rounded-md overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={tajMahal} alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
