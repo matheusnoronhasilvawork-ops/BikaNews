587f9d04aab5f5cd5fd418d9557699d4ae4d2817
= <=>
2026-04-10 09:44:18 -0300

    style: responsividade no header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import './App.css'
 import bikanews from './assets/bikanews.png'
-import { Search, Bell, User } from 'lucide-react'
+import { Search, User, Menu } from 'lucide-react'
 //#fa6832
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
 
-      <header className="w-full h-16 flex items-center justify-center">
-        <div className="flex-1 h-full flex justify-evenly items-center px-20">
+      <header className="w-full h-16 flex items-center">
+        <div className="flex-1 h-full flex gap-7 items-center pl-4">
+          <Menu className="min-w-8 min-h-8 cursor-pointer xl:hidden" />
           <img src={bikanews} alt="logo" className="w-32 h-24" />
-          <nav className="font-bold text-lg cursor-pointer">Inicio</nav>
-          <nav className="font-bold text-lg cursor-pointer">Política</nav>
-          <nav className="font-bold text-lg cursor-pointer">Economia</nav>
-          <nav className="font-bold text-lg cursor-pointer">Esporte</nav>
-          <nav className="font-bold text-lg cursor-pointer">Tecnologia</nav>
+          <nav className="font-bold text-lg cursor-pointer max-xl:hidden">Inicio</nav>
+          <nav className="font-bold text-lg cursor-pointer max-xl:hidden">Política</nav>
+          <nav className="font-bold text-lg cursor-pointer max-xl:hidden">Economia</nav>
+          <nav className="font-bold text-lg cursor-pointer max-xl:hidden">Esporte</nav>
+          <nav className="font-bold text-lg cursor-pointer max-xl:hidden">Tecnologia</nav>
         </div>
 
         <div className="w-[25%] h-full flex justify-center items-center gap-12">
-          <Search className="w-8 h-8 cursor-pointer" />
-          <Bell className="w-8 h-8 cursor-pointer" />
-          <User className="w-8 h-8 cursor-pointer" />
+          <Search className="min-w-8 min-h-8 cursor-pointer" />
+          <User className="min-w-8 min-h-8 cursor-pointer" />
         </div>
       </header>
     </div>
