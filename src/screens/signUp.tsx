99d63138252b988becf490bb5c8807398cbbdb12
Matheus Noronha da Silva <matheusnoronhasilva.work@gmail.com>
2026-04-22 17:20:55 -0300

    style: adição de circulos na borda e remoção da logo

--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -47,29 +47,30 @@ export default function SignUp() {
     }
 
     return (
-        <div className="h-screen bg-gray-50 pb-5 pt-2  flex flex-col items-center justify-center">
+        <div className="h-screen bg-gray-100 pb-5 pt-2  flex flex-col items-center overflow-hidden justify-center relative">
+            <div className="bg-[#fa6732] h-180 -bottom-100 -left-80 w-180 rounded-full absolute" />
+            <div className="bg-[#fa6732] h-180 -top-100 -right-80 w-180 rounded-full absolute" />
             <div className="w-100 h-full items-center justify-center flex flex-col">
-                <img className="w-30" src={BikaNews} alt="Bika News" />
                 <div className="flex flex-col items-center justify-center gap-2 mb-5">
                     <h1 className="text-3xl font-bold">Crie sua conta</h1>
                     <h2 className="text-sm">Cadastre-se e comece a escrever hoje mesmo</h2>
                 </div>
                 <div className="flex flex-col w-full bg-white px-6 py-4 h-fit rounded-2xl shadow-md">
-                    <form onSubmit={handleSubmit} action="createUser" className="flex flex-col h-full w-full gap-4 items-center">
-                        <div className="flex flex-col gap-2 w-full h-8 mb-4">
+                    <form onSubmit={handleSubmit} action="createUser" className="flex flex-col h-full w-full gap-2 items-center">
+                        <div className="flex flex-col gap-2 w-full h-fit">
                             <span className="text-sm font-medium">Nome completo</span>
                             <div className="flex flex-row border h-full border-gray-300 rounded-lg p-3 gap-3 items-center">
                                 <input className="outline-none w-full text-sm" placeholder="Seu nome" value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
                         </div>
-                        <div className="flex flex-col gap-2 w-full h-8 mb-4">
+                        <div className="flex flex-col gap-2 w-full h-fit">
                             <span className="text-sm font-medium">Email</span>
                             <div className="flex flex-row border h-full border-gray-300 rounded-lg p-3 gap-3 items-center">
                                 <Mail size={15} className="text-gray-400" />
                                 <input className="outline-none w-full text-sm" placeholder="seu@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
                         </div>
-                        <div className="flex flex-col gap-2 w-full h-8 mb-4">
+                        <div className="flex flex-col gap-2 w-full h-fit">
                             <span className="text-sm font-medium">Senha</span>
                             <div className="flex flex-row border h-full border-gray-300 rounded-lg p-3 gap-3 items-center">
                                 <Lock size={20} className="text-gray-400" />
@@ -77,7 +78,7 @@ export default function SignUp() {
                                 <Eye size={20} className="text-gray-400 outline-none display-none ml-auto" />
                             </div>
                         </div>
-                        <div className="flex flex-col gap-2 w-full h-8 mb-8">
+                        <div className="flex flex-col gap-2 w-full h-fit">
                             <span className="text-sm font-medium">Confirmar senha</span>
                             <div className="flex flex-row border h-full border-gray-300 rounded-lg p-3 gap-3 items-center">
                                 <Lock size={20} className="text-gray-400" />
@@ -85,7 +86,7 @@ export default function SignUp() {
                                 <Eye size={20} className="text-gray-400 outline-none display-none ml-auto" />
                             </div>
                         </div>
-                        <button className="text-white bg-[#fa6732] h-10 hover:bg-[#c64f24] hover:scale-103 transition duration-300 rounded-lg px-4 py-2 w-full flex flex-row items-center justify-center gap-2 cursor-pointer">
+                        <button className="text-white bg-[#fa6732] h-10 hover:bg-[#c64f24] hover:scale-103 transition duration-300 rounded-lg px-4 py-2 w-full flex flex-row mt-2 items-center justify-center gap-2 cursor-pointer">
                             <UserRoundPlus size={20} />
                             <span>Criar conta</span>
                         </button>
