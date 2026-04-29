ae4c34f48f90b083826a162c3f96ced7e128f7d9
Matheus Noronha da Silva <matheusnoronhasilva.work@gmail.com>
2026-04-29 10:05:36 -0300

    feat: adicionando hash ao cadastro

--- src/back-database/models/signUp-model.js
+++ src/back-database/models/signUp-model.js
@@ -2,10 +2,10 @@ import db from '../db.js';
 import bcrypt from 'bcrypt';
 
 async function signUp(name, email, password, role_id, phone, state_id, city_id) {
-    // const hash = await bcrypt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
 
     const query = 'INSERT INTO users (name, email, password, role_id, phone, state_id, city_id) VALUES (?, ?, ?, ?, ?, ?, ?)';    
-    return db.promise().query(query, [name, email, password, role_id, phone, state_id, city_id]);
+    return db.promise().query(query, [name, email, hashPassword, role_id, phone, state_id, city_id]);
 };
 
 async function findUserByEmail(email) {
